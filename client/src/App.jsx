60cacb0a68877c0577c3e9bbb744654c79158ca1
Aryan Bola <aaryann5002@gmail.com>
2025-05-18 17:41:22 +0530

    now working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,21 +4,42 @@ function App() {
 
   const [data, setData] = useState([])
 
-  useEffect(()=>{
+  useEffect(() => {
     const apiCall = async () => {
-      const response = await fetch(`http://localhost:3000/`)
-      const resData = await response.json() 
-      resData
-        .then(()=>setData(resData))
-      console.log(data)
-    }
-    apiCall()
-  },[])
+      try {
+        const response = await fetch(`http://localhost:3000/`);
+        const resData = await response.json();
+
+        setData(resData);
+
+        console.log("Fetched data:", resData);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+
+    apiCall();
+  }, []);
 
   return (
     <>
-      <div className="bg-black h-screen">
-        {/* <p className="text-white">{data[0].headline.main}</p> */}
+      <div className="bg-black h-screen flex flex-col p-2 gap-4">
+        <div className="border border-white rounded-xl p-2 gap-4 grid">
+          <p className="text-white text-xl">{data[0]?.headline.main || "Loading..."}</p>
+          <p className="text-white">{data[0]?.abstract}</p>
+          <p className="text-gray-500">{data[0]?.byline.original}</p>
+          <img src={`${data[0]?.multimedia[3].legacy.thumbnail }`} alt="" />
+        </div>
+      
+        { data.map((items)=>(
+          <div className="bg-black h-screen flex flex-col p-2 gap-4" key={items._id}>
+            <div className="border border-white rounded-xl p-2 gap-4 grid">
+              <p className="text-white text-xl">{items?.headline.main || "Loading..."}</p>
+              <p className="text-white">{items?.abstract}</p>
+              <p className="text-gray-500">{items?.byline.original}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   )
